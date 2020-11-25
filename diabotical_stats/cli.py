5a0a53bf4fd7bcb4990567a02dc939d739273714
Andrey Martsenius <artlynee@gmail.com>
2020-11-25 22:41:13 +0500

    fix: argument 'count' only takes positive numbers

--- diabotical_stats/cli.py
+++ diabotical_stats/cli.py
@@ -4,12 +4,19 @@ GAME_MODES = ('r_macguffin', 'r_wo', 'r_rocket_arena_2', 'r_shaft_arena_1',
               'r_ca_2', 'r_ca_1')
 
 
+def check_positive(x):
+    value = int(x)
+    if value < 0:
+        raise argparse.ArgumentTypeError('Only positive numbers available.')
+    return value
+
+
 def parser():
     parser = argparse.ArgumentParser(description='Get players stats from \
                                         game Diabotical')
     parser.add_argument('--mode', choices=GAME_MODES, required=True,
                         help='choose game mode')
-    parser.add_argument('--count', type=int,
+    parser.add_argument('--count', type=check_positive,
                         help='specify the number of records to output')
     parser.add_argument('--user_id',
                         help='specify the user id you want to find')
